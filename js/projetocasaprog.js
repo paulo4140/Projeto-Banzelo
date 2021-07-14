bc457f463a4c1c7607e9c29b8dd0fb873ea428e8
paulo4140 <paulocarvalho16443@gmail.com>
2021-07-14 01:30:42 +0100

    first commit

---
+++ js/projetocasaprog.js
@@ -0,0 +1,8 @@
+function modonoturno(){
+document.getElementById('at').style.background='black'
+}
+function modomanha(){
+    document.getElementById('at').style.background='#fff'
+    
+    
+    }
